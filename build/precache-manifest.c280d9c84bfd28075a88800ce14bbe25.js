853a7810726454e21278876fb366b3835e8a2aef
viwnj <vitorhprz@gmail.com>
2018-11-28 18:01:17 -0200

    adding production build

---
+++ build/precache-manifest.c280d9c84bfd28075a88800ce14bbe25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6c7e4b464b5f47c14e",
+    "url": "/desafio-pushstart/static/js/runtime~main.8c6c7e4b.js"
+  },
+  {
+    "revision": "b4231b7a2713163b9313",
+    "url": "/desafio-pushstart/static/js/main.b4231b7a.chunk.js"
+  },
+  {
+    "revision": "8d5d8057d009c73a3d62",
+    "url": "/desafio-pushstart/static/js/1.8d5d8057.chunk.js"
+  },
+  {
+    "revision": "b4231b7a2713163b9313",
+    "url": "/desafio-pushstart/static/css/main.828a4f59.chunk.css"
+  },
+  {
+    "revision": "04727ab2bba9ecef292accdb058580a1",
+    "url": "/desafio-pushstart/index.html"
+  }
+];
